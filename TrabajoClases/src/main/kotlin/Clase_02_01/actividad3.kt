87ca7fd5c0a42ff6d96544fa11f15d02042d782b
IvanniaEdith <ivannia.arellano@cetys.edu.mx>
2019-02-01 19:55:39 -0800

    Nuevo package Clase_02_01 numeros palindromos

---
+++ TrabajoClases/src/main/kotlin/Clase_02_01/actividad3.kt
@@ -0,0 +1,22 @@
+package Clase_02_01
+
+class actividad3 {
+    fun isPalindrome(number: Int):Boolean{
+        if(number < 0){
+            return false
+        }
+        if(number % 10 == 0 && number != 0){
+            return false
+        }
+
+        var tmp1: Int = number
+        var reverse = 0
+
+        while (tmp1 > reverse){
+            reverse = reverse * 10 + tmp1 % 10
+            tmp1 /= 10
+        }
+
+        return (tmp1 == reverse || tmp1 == reverse / 10)
+    }
+}
---
+++ TrabajoClases/src/test/kotlin/Clase_02_01/actividad3test.kt
@@ -0,0 +1,30 @@
+package Clase_02_01
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+
+class actividad3test {
+    private val actividad3 = actividad3()
+
+    @Test
+    fun `return false when number is negative`(){
+        val number = -100
+        val expected = false
+        assertFalse(actividad3.isPalindrome(number))
+    }
+
+    @Test
+    fun `return false when number ends in zero and the number is not zero`(){
+        val number = 1000
+        assertFalse(actividad3.isPalindrome(number))
+    }
+
+    @Test
+    fun `return false when numbers are not palindrome`(){
+        val number = 89765
+        assertFalse(actividad3.isPalindrome(number))
+    }
+
+
+}
